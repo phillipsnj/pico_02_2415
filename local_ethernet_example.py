16f100dd2217fbe9043187186edc41a0157d2077
Nigel Phillips <nigeljphillips@yahoo.co.uk>
2026-02-15 18:10:55 +0000

    Added an example that can be run locally on a PC.

--- local_ethernet_example.py
+++ local_ethernet_example.py
@@ -1,5 +1,7 @@
+# This is an example of using the base CBUS class on your PC.
+# Requires a Cbus network connection on the same PC
+
 from lib.eth_node import eth_cbus_node
-# import socket
 import asyncio
 
 def process_message(msg):
@@ -7,11 +9,8 @@ def process_message(msg):
     for event in msg['info']:
         local_node.acon(event) if msg['status'] == 'on' else local_node.acof(event)
 
-#cbus_header = ':SB060N'
 local_node = eth_cbus_node(600, process_message, "localhost", 5550)
 
-# cbus_ethernet.start()
-#local_node.send(f'{cbus_header}0D;')
 local_node.teach_long_event(300, 9, [1,10])
 local_node.pnn()
 local_node.acon(1)
@@ -20,6 +19,5 @@ local_node.acon(1)
 loop = asyncio.new_event_loop()
 
 loop.create_task(local_node.run())
-# loop.create_task(node.print_details())
 
 loop.run_forever()
